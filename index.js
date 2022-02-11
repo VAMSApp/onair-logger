21800e41310f2d186e778a372bcef4f36cfde6e3
Mike DeVita <mike@devita.co>
2022-02-11 10:44:08 -0700

    added some logging, this needs to be abstracted & cleaned up.

--- index.js
+++ index.js
@@ -13,50 +13,95 @@ const companyRepo = require('./lib/repos/company');
     const Api = new OnAirApi(apiConfig);
 
     async function refreshCompanyDetails() {
+        console.log(`Started refreshCompanyDetails`);
+
         return Api
             .getCompany()
             .then(async function processCompany(onair_company) {
                 const { AirlineCode, Id, WorldId, } = onair_company;
 
-                console.log(`Started Processing ${AirlineCode} Company`);
-                console.log(`  Looking up world by World ID: ${WorldId}`);
+                console.log(`  Started Processing ${AirlineCode} Company`);
+                console.log(`    Looking up world by World ID: ${WorldId}`);
 
                 const world = await worldRepo.findByUuid(WorldId);
 
                 if (world) {
-                    console.log(`  World found: ${world.ShortName}`);
+                    console.log(`    World found: ${world.ShortName}`);
                 }
 
-                console.log(`  Trying to find the Company in the database by the provided Company UUID: ${Id}`);
+                console.log(`    Trying to find the Company in the database by the provided Company UUID: ${Id}`);
                 let company = await companyRepo.findByUuid(Id);
 
                 if (company) {
-                    console.log(`  Company UUID ${company.Uuid} found in database.`);
+                    console.log(`    Company UUID ${company.Uuid} found in database.`);
 
-                    console.log(`  Refreshing ${company.AirlineCode} (${company.Id}) with the latest data.`);
+                    console.log(`    Refreshing ${company.AirlineCode} (${company.Id}) with the latest data.`);
                     company = await companyRepo.update(company.Id, onair_company);
 
                 } else {
-                    console.log(`  Company UUID ${Id} was not found in the database.`);
+                    console.log(`    Company UUID ${Id} was not found in the database.`);
 
-                    console.log('  Creating the new Company');
+                    console.log('    Creating the new Company');
                     company = await companyRepo.create(onair_company);
 
-                    console.log(`  Company ${company?.AirlineCode} created. Company ID is ${company?.Id}.`);
+                    console.log(`    Company ${company?.AirlineCode} created. Company ID is ${company?.Id}.`);
                 }
 
-                console.log(`Finished Processing Company (${company.Id}) ${AirlineCode}.`);
+                console.log(`  Finished Processing Company (${company.Id}) ${AirlineCode}.`);
+
 
                 return company;
+            })
+            .then(() => {
+                console.log('Finished refreshCompanyDetails()');
+                console.log();
             })
             .catch(function errorProcessingCompany(err) {
                 console.error('Error processing company');
                 console.error(err);
+                throw err;
             });
     }
 
     async function refreshCompanyFleet() {
-        console.log('refreshCompanyFleet finished.')
+        console.log(`Started refreshCompanyFleet`);
+
+        let Created = [];
+        let Updated = [];
+
+        return Api
+            .getCompanyFleet()
+            .then(async function processCompanyFleet(onair_fleet) {
+                onair_fleet.forEach(async function eachAircraft (oa_aircraft) {
+
+                    const {
+                        Id,
+                        AircraftTypeId,
+                        AircraftClassId,
+                        Identifier,
+                    } = oa_aircraft;
+
+                    // try to find the aircraft by Uuid in the database
+
+                    // if aircraft exists
+                        // update it
+                        // push the aircraft Id into the Updated array
+                    // otherwise
+                        // create it
+                        // push the aircraft Id into the Created array
+
+                });
+
+            })
+            .then(() => {
+                console.log('Finished refreshCompanyFleet()');
+                console.log();
+            })
+            .catch(function errorProcessingCompanyFleet(err) {
+                console.error('Error processing Company Fleet');
+                console.error(err);
+                throw err;
+            });
     }
 
     const runList = [
@@ -64,5 +109,18 @@ const companyRepo = require('./lib/repos/company');
         refreshCompanyFleet
     ];
 
-    await Promise.all(runList.map(fn => fn()))
+    // run concurrently
+    // await Promise.all(runList.map(fn => fn()))
+
+    // run in series
+    for (const fn of runList) {
+        await fn();
+    }
+
 })();
+
+
+// Id: 'd8a921a5-4774-4530-b75e-14b41545cabc',
+// AircraftTypeId: 'da06adca-0e5b-43f9-b317-fa3a89d7fc05',
+// AircraftClassId: '04422853-db1a-425b-85c8-fdf3fdb54c66',
+// Identifier:
